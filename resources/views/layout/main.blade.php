8432db17f994f09917dcb0b399a7f97685cc7a5d
dwiyandani <yandanidwi@gmail.com>
2019-10-05 01:57:15 +0700

    mengubah url dasboard, mengganti judul menu menjadi management karyawan, mengubah submenu component yang ada di menu management karyawan menjadi karyawan, dan mengubah submenu button yang ada didalam submenu karyawan menjadi list karyawan

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -48,7 +48,7 @@
     <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
 
         <!-- Sidebar - Brand -->
-        <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
+        <a class="sidebar-brand d-flex align-items-center justify-content-center" href="{{ url('/') }}">
           <div class="sidebar-brand-icon rotate-n-15">
             <i class="fas fa-laugh-wink"></i>
           </div>
@@ -60,7 +60,7 @@
 
         <!-- Nav Item - Dashboard -->
         <li class="nav-item active">
-          <a class="nav-link" href="index.html">
+          <a class="nav-link" href="{{ url('/') }}">
             <i class="fas fa-fw fa-tachometer-alt"></i>
             <span>Dashboard</span></a>
         </li>
@@ -70,19 +70,19 @@
 
         <!-- Heading -->
         <div class="sidebar-heading">
-          Interface
+          Management Karyawan
         </div>
 
         <!-- Nav Item - Pages Collapse Menu -->
         <li class="nav-item">
           <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">
             <i class="fas fa-fw fa-cog"></i>
-            <span>Components</span>
+            <span>Karyawan</span>
           </a>
           <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
             <div class="bg-white py-2 collapse-inner rounded">
               <h6 class="collapse-header">Custom Components:</h6>
-              <a class="collapse-item" href="buttons.html">Buttons</a>
+              <a class="collapse-item" href="{{ url('/karyawan') }}">List Karyawan</a>
               <a class="collapse-item" href="cards.html">Cards</a>
             </div>
           </div>
