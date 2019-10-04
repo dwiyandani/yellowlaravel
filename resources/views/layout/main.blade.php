af89599771f6c367e87238e3a00200a92ba526a0
dwiyandani <yandanidwi@gmail.com>
2019-10-05 02:01:48 +0700

    menghapus navbar

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -22,24 +22,6 @@
   <body id="page-top">
 
 
-
-  <!-- navbar -->
-            <nav class="navbar navbar-expand-lg navbar-light bg-light">
-                <div class="container">
-                    <a class="navbar-brand" href="#">Navbar</a>
-                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
-                    </button>
-                    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-                        <div class="navbar-nav">
-                        <a class="nav-item nav-link active" href="{{ url('/') }}">Home</a>
-                        <a class="nav-item nav-link" href="{{ url('/about') }}">About</a>
-                        <a class="nav-item nav-link" href="{{ url('/karyawan') }}">Karyawan</a>
-                        </div>
-                    </div>
-                </div>
-            </nav>
-<!-- end Navbar -->
     @section('sidebar')
 
     <!-- Page Wrapper -->
